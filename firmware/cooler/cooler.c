0006cb27274fec33a487268e05236c76c13cad61
Vladimir Karamyshev <quangorn@gmail.com>
2019-02-24 15:12:49 +0300

    temp commit

--- firmware/cooler/cooler.c
+++ firmware/cooler/cooler.c
@@ -4,41 +4,35 @@
 #define MIN_FAN_SPEED 50 //20%
 
 void enableFan() {
-	TCCR0A |= 1 << COM0A1;
+	TCCR2A |= 1 << COM2A1;
 }
 
 void disableFan() {
-	TCCR0A &= ~(1 << COM0A1);
+	TCCR2A &= ~(1 << COM2A1);
 }
 
 void coolerInit() {
-	//PD6 (OC0A) is now an output //TODO: PB3 (OC2A)
-	DDRD |= 1 << DDD6;
-	//PB1 (OC1A) is now an output
-	DDRB |= 1 << DDB1;
+	//PB1(OC1A), PB2, PB3(OC2A) are now an output
+	DDRB |= (1 << DDB1) | (1 << DDB2) | (1 << DDB3);
 
 	//set none-inverting mode and 8bit fast PWM Mode
-	TCCR0A |= (1 << WGM01) | (1 << WGM00);
-	//set none-inverting mode and 8bit fast PWM Mode, enable PWM on channel A
-	TCCR1A |= (1 << WGM10) | (1 << COM1A1);
+	TCCR2A |= (1 << WGM21) | (1 << WGM20);
+	//set inverting mode and 8bit fast PWM Mode, enable PWM on channel A
+	TCCR1A |= (1 << WGM10) | (1 << COM1A0) | (1 << COM1A1);
 
 	//set prescaler to 1024
-	TCCR0B |= (1 << CS00) | (1 << CS02);
+	TCCR2B |= (1 << CS20) | (1 << CS21) | (1 << CS22);
 	//set prescaler to 8
 	TCCR1B |= (1 << WGM12) | (1 << CS11);
-
-	//cooler power is inverted
-	OCR1A = 0xFF;
 }
 
 void coolerSetPower(uint8_t power) {
 	if (power >= MIN_FAN_SPEED) {
-		OCR0A = power;
+		OCR2A = power;
 		enableFan();
 	} else {
 		disableFan();
 	}
 
-	//cooler power is inverted
-	OCR1A = 0xFFu - power;
+	OCR1A = power;
 }
